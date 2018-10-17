16b5e0a1578c8c6a7c99d259ccf5553fbf05251e
ferbanahe <ferba6@yahoo.es>
2018-10-17 12:19:17 +0200

    Cambios definitivos en app.jss e index.html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-documents.addEventlistener('DOMContentLoaded', () => {})
+documents.addEventlistener('DOMContentLoaded', () => {
+    const msg = 'Terminada la carga de DOM'
+    console.log(msg)
+})
